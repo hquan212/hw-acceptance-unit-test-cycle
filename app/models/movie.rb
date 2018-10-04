224ed6eb8ed8ef295b88259011ed6cb3e00c00d8
hquan212 <rhq66@tamu.edu>
2018-10-04 01:22:55 +0000

    heroku push

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ActiveRecord::Base
+  def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+  end
+  
+  def samedirector
+    Movie.where(:director => director) if (not director.nil?) and (not director.empty?)
+  end
+end
